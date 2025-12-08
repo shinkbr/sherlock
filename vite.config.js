dfb8cc7c762474455ad907645facf14c4cf59eea
Shintaro Kobori <1741788+shinkbr@users.noreply.github.com>
2025-12-08 15:07:25 +0000

    change path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    base: '/',
+    base: '/sherlock/',
     plugins: [
         react(),
         tailwindcss(),
